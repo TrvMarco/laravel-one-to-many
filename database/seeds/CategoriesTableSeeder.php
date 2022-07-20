6a4bbc81284db1c9b75208c899d3a93771307ede
TrvMarco <trevisanello.marco@gmail.com>
2022-07-20 17:48:32 +0200

    creato seeder per categorie

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Category;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Front End', 'Back End', 'DevOps', 'Graphic Design', 'Web Design'];
+        
+        foreach($categories as $category){
+            $newCategory = new Category();
+            $newCategory->name = $category;
+            $newCategory->slug = Str::of($category)->slug('-');
+            $newCategory->save();
+        }
+    }
+}
